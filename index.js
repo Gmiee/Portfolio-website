9bf62482a1deb5a36f11b4288decb86e0a7861ef
Khemnani Jenish <104885605+Gmiee@users.noreply.github.com>
2023-10-31 19:24:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const spinner = document.querySelector(".newtons-cradle");
+window.addEventListener('load',()=>{
+    spinner.style.opacity = '0';
+    spinner.style.body.display='none';
+})
+const ld = document.querySelector("body");
+window.addEventListener('load',()=>{
+    ld.style.body.display = 'hidden';
+})
